58693ff1147bf007cfc4e37abaa95acb8204777a
arturyahnychuk <arturyahnychuk@gmail.com>
2023-10-30 16:16:55 +0200

    added loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
+import { useState, useEffect } from 'react'
 import { RouterProvider } from 'react-router-dom'
 import Header from "@/components/common/Header"
 import PageLayout from "@/components/layout/PageLayout"
+import Loader from '@/components/loader/Loader'
 import router from "@/router"
 import '@/index.css'
 
-const App = () => {  
+const App = () => {
+
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    window.onload = () => {
+      setIsLoading(false)
+    }
+
+    return () => {
+      window.onload = null
+    }
+  }, [])
+
   return (
     <>
       <PageLayout>
+        { isLoading && (<Loader />)}
         <Header />
         <RouterProvider router={router} />
       </PageLayout>      
